bde9cdd90718442582175151b5ff5b53b3661b7e
Bilal-01 <mbilalaziz.01@gmail.com>
2022-11-09 18:24:37 +0500

    file upload

--- src/components/FileUpload.jsx
+++ src/components/FileUpload.jsx
@@ -1,17 +1,19 @@
-import React, { useRef } from 'react';
+import React, { useRef, useContext } from 'react';
 import { Button, Typography } from '@mui/material';
 import FileUploadIcon from '@mui/icons-material/FileUpload';
 
-function FileUpload(){
-    
+function FileUpload(props){
+    var arr;
     const file= useRef(null);
-    
     const handleFile = () => {
         file.current.files[0].arrayBuffer().then((buffer) => {
-            const list = new TextDecoder('utf-8').decode(buffer)
-            list.forEach()
+            const list = new TextDecoder('utf-8').decode(buffer);
+            arr = list.split(' ').map(function(item) {
+                return parseInt(item);
+            });
         })
     }
+    props.onHandle(10);
     
     return (
         <div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,24 +1,19 @@
-import React from 'react';
+import React, { useState, createContext} from 'react';
 import Bars from './Bars';
 import FileUpload from './FileUpload';
 import '../css/home.css';
 
 /*
-    RESTRICTIONS
+RESTRICTIONS
         1. Negative Numbers
         2. Real Numbers
         3. Range : 1 - 1000
 */
 
 function Home() {
-
-    const arr = [99, 199, 299, 399, 50, 2, 120, 20, 500];
-    var avg = 0;
-
-    arr.forEach(function(item, index) {
-        avg += item;
-    });
-    avg = avg / arr.length;
+    const [arr, setArr] = useState([0]);
+    const [x, setX] = useState(0);
+    console.log(arr);
 
     return(
         <>
@@ -26,26 +21,26 @@ function Home() {
             <div className ='rowC'>
                 <div className='home-container'>
                     {
-                        arr.map((value, i) => {
-                            return(
-                                <div key = {i} style={{textAlign: 'center'}}>
-                                    <h3 style={{color: '#205375', marginBottom: '0px' }}>{value}</h3>
-                                    <Bars num = {value} size={arr.length} avg={avg} />
-                                </div>
-                            ) 
-                        })
+                        // arr.map((value, i) => {
+                        //     return(
+                        //         <div key = {i} style={{textAlign: 'center'}}>
+                        //             <h3 style={{color: '#205375', marginBottom: '0px' }}>{value}</h3>
+                        //             <Bars num = {value} size={arr.length} />
+                        //         </div>
+                        //     ) 
+                        // })
                     }
                 </div>
                 <div>
                     <div className='code-container'>
                         {
-                            arr.map((value, i) => {
-                                return(
-                                    <div key={i}>
-                                        Hello
-                                    </div>
-                                ) 
-                            })
+                            // arr.map((value, i) => {
+                            //     return(
+                            //         <div key={i}>
+                            //             Hello
+                            //         </div>
+                            //     ) 
+                            // })
                         }
                     </div>
                     <div className="complexity-container">
@@ -59,7 +54,7 @@ function Home() {
                 </div>
             </div>
             <div className='file-upload'>
-                 <FileUpload />
+                <FileUpload handleChange={setArr} onHandle={setX} />
             </div>
         </div>
         </>
